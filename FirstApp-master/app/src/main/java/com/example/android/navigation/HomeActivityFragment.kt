4d97386128df9c7da7acdd1c222f3dc4ef3478dc
LifeStly <tattonman001@gmail.com>
2020-08-16 05:30:17 +0700

    navigation

--- FirstApp-master/app/src/main/java/com/example/android/navigation/HomeActivityFragment.kt
+++ FirstApp-master/app/src/main/java/com/example/android/navigation/HomeActivityFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AddFragment.newInstance] factory method to
+ * Use the [HomeActivityFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddFragment : Fragment() {
+class HomeActivityFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,7 +32,7 @@ class AddFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_home_activity, container, false)
     }
 
     companion object {
@@ -42,12 +42,12 @@ class AddFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AddFragment.
+         * @return A new instance of fragment HomeActivityFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-                AddFragment().apply {
+                HomeActivityFragment().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
--- FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/add/AddFragment.kt
+++ FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/add/AddFragment.kt
@@ -1,17 +1,19 @@
-package com.example.android.navigation
+package com.example.android.navigation.fragment.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.android.navigation.R
 
-class ListFragment : Fragment() {
+class AddFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_add, container, false)
     }
+
 
 }
