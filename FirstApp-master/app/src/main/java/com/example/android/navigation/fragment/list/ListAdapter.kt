2fda20c8bdb8db6ad9758a04fb07a1c599f97074
LifeStly <tattonman001@gmail.com>
2020-08-16 09:58:58 +0700

    listadpter

--- FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/list/ListAdapter.kt
+++ FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/list/ListAdapter.kt
@@ -6,16 +6,22 @@ import android.view.ViewGroup
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.appcompat.widget.DecorContentParent
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.navigation.R
 import com.example.android.navigation.data.User
+import kotlinx.android.synthetic.main.custom_row.view.*
 
 class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
     private  var userList = emptyList<User>()
 
     class MyViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){ }
 
-    override fun onCreateViewHolder(parent: ViewGroup):MyViewHolder{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row,parent,false))
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, posiyion)
+    override fun onBindViewHolder(holder: MyViewHolder, posiyion: Int){
+        val currentItem = userList[position]
+        holder.itemView.id_txt.text = currentItem.id.toString()
+        holder.itemView.firstName_txt.text = currentItem.id.toString()
+    }
 }
