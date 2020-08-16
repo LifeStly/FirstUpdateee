e45bbfeff62fa133e8f5c8fa05f182fb0631fbc1
LifeStly <tattonman001@gmail.com>
2020-08-16 10:11:48 +0700

    finaddRead

--- FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/list/ListFragment.kt
+++ FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/list/ListFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.navigation.R
 import com.example.android.navigation.data.UserViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -20,7 +23,15 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
 
-        val
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+            adapter.setData(user)
+        })
         view.floatingActionButton.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
