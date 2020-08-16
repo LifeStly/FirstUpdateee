9fac866f0515cb4ed556a6d91879200f6e06a71d
LifeStly <tattonman001@gmail.com>
2020-08-16 10:04:57 +0700

    listadapter

--- FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/list/ListAdapter.kt
+++ FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/list/ListAdapter.kt
@@ -18,10 +18,19 @@ class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row,parent,false))
     }
+    override fun getItemCount(): Int {
+        return userList.size
+    }
 
-    override fun onBindViewHolder(holder: MyViewHolder, posiyion: Int){
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int){
         val currentItem = userList[position]
         holder.itemView.id_txt.text = currentItem.id.toString()
         holder.itemView.firstName_txt.text = currentItem.id.toString()
     }
+    fun setData(user: List<User>){
+        this.userList = user
+        notifyDataSetChanged()
+    }
+
+
 }
--- FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/list/ListFragment.kt
+++ FirstApp-master/app/src/main/java/com/example/android/navigation/fragment/list/ListFragment.kt
@@ -7,16 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.android.navigation.R
+import com.example.android.navigation.data.UserViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
 class ListFragment : Fragment() {
+
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
 
+        val
         view.floatingActionButton.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
